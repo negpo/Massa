3e168ce7e9c76eb7a86dae4d45e28af7e2993553
Dimokus <Dimokus@mail.ru>
2022-04-19 20:24:27 +0300

    Update rolls.sh

--- rolls.sh
+++ rolls.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
-
-
 cd /massa/massa-client
-
 while true
 do
         tail /massa/massa-node/nohup.out
 		
-		./massa-client node_testnet_rewards_program_ownership_proof $my_wallet_addr $my_discord_id
-		
-	    balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+	./massa-client node_testnet_rewards_program_ownership_proof $my_wallet_addr $my_discord_id
+	balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
         int_balance=${balance%%.*}
-		
-		
-        if [[ "$int_balance" -gt "99" ]] ; then
+	if [[ "$int_balance" -gt "99" ]] ; then
                 echo "More than 99. Баланс токенов более 99. "
                 resp=$(./massa-client buy_rolls $my_wallet_addr $(($int_balance/100)) 0)
                 echo $resp
