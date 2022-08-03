f47d010c2a4986623134f803dec48b1b9df7a93d
Dimokus <Dimokus@mail.ru>
2022-08-03 10:40:04 +0300

    Update massa_start.sh

--- massa_start.sh
+++ massa_start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+source $HOME/.bashrc
 TZ=Europe/Kiev
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
 apt-get upgrade -y
-apt install sudo wget curl tar nano sshd -y
-
+apt-get install -y sudo nano wget tar zip unzip jq ssh git
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
+
+sleep 5
+sudo apt-get install -y nano runit
+runsvdir -P /etc/service &
+source $HOME/.bashrc
 discord=1
 echo 'export my_root_password='${my_root_password} >> $HOME/.bashrc
 echo 'export my_discord_id='${my_discord_id} >> $HOME/.bashrc
