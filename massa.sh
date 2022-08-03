72fd25251631d343b1b5f60e4fac08d44bd8e822
Dimokus <Dimokus@mail.ru>
2022-08-03 11:00:55 +0300

    Create massa.sh

---
+++ massa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source $HOME/.bashrc
+TZ=Europe/Kiev
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+apt-get update
+apt-get upgrade -y
+apt-get install -y sudo nano wget tar zip unzip ssh  git 
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+service ssh restart
+sleep 5
+sudo apt-get install -y nano runit
+runsvdir -P /etc/service &
+source $HOME/.bashrc
